09f863a12bb8bad771934a098a77ec2f591bbc31
Seth Battis <sbattis@groton.org>
2025-03-30 15:05:58 -0400

    fix(slim-lti-infrastructure-gae): over-zealous types

--- src/Cache.php
+++ src/Cache.php
@@ -6,8 +6,8 @@ namespace GrotonSchool\Slim\LTI\Infrastructure\GAE;
 
 use Exception;
 use Google\Cloud\Firestore\FirestoreClient;
-use GrotonSchool\Slim\LTI\Domain\ConsumerConfigurationInterface;
 use GrotonSchool\Slim\LTI\Infrastructure\CacheInterface;
+use JsonSerializable;
 
 /**
  * @see https://github.com/packbackbooks/lti-1-3-php-library/wiki/Laravel-Implementation-Guide#cache Working from Packback's wiki example
@@ -118,7 +118,7 @@ class Cache implements CacheInterface
     }
 
     public function cacheRegistrationConfiguration(
-        ConsumerConfigurationInterface $config,
+        JsonSerializable $config,
         string $registration_token
     ): string {
         $document = $this->firestore->collection(self::COLLECTION_PATH)->newDocument();
