6a70f2dbfd6e1965ed8e2a7a89a460c610104843
Seth Battis <sbattis@groton.org>
2025-08-07 13:57:36 -0400

    feat: inject dependencies with Dependencies::injec()

---
+++ src/Dependencies.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GrotonSchool\Slim\LTI\Infrastructure\GAE;
+
+use DI;
+use DI\ContainerBuilder;
+use GrotonSchool\Slim\LTI\Infrastructure\CacheInterface;
+use GrotonSchool\Slim\LTI\Infrastructure\DatabaseInterface;
+
+class Dependencies
+{
+    public static function inject(ContainerBuilder $containerBuilder)
+    {
+        $containerBuilder->addDefinitions([
+            // autowire groton-school/slim-lti-shim implementations
+            CacheInterface::class => DI\autowire(Cache::class),
+            DatabaseInterface::class => DI\autowire(Database::class)
+        ]);
+    }
+}
